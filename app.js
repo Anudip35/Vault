f0d121c2b21fea5ffe1cc659d1561f4e701a6258
Anudip <anudipdg2003@gmail.com>
2022-08-14 22:10:15 +0530

    port environment var changed

--- app.js
+++ app.js
@@ -173,6 +173,6 @@ app.post("/login", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started successfully!");
 });
